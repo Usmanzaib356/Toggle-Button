f27460861217cb655feada61dc6e447bfa0a82df
usmanzaib <usmanzaib356@gmail.com>
2023-04-29 05:26:48 -0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import "./index.css"
 function App() {
 
-  const [theme, settheme] = useState(false)
+  const [theme, settheme] = useState( localStorage.getItem('theme') === 'dark' ? true : false)
 
   function toggle() {
     settheme(!theme)
   }
+
+
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme ? 'dark' : 'light');
+  }, [theme]);
+
 
   return (
     <>
       <div className={theme ? 'containerdark' : 'containerlight'}>
         <h1> {theme ? "Dark Theme" : "Light Theme"} </h1>
         <label class="switch">
-          <input type="checkbox" class="checkbox" onClick={toggle} />
+          <input type="checkbox" class="checkbox" onClick={toggle} checked={theme} />
           <div class="slider"></div>
         </label>
       </div>
